0d1d61256e6fade652632811974ad4eb52c35652
timi95 <timi.ogunkeye@gmail.com>
2017-12-07 15:01:42 +0100

    this ought to work about now ???

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -13,7 +13,7 @@ import os
 import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,7 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    }#.update(dj_database_url.config(conn_max_age=500))
+
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -112,7 +113,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Change 'default' database configuration with $DATABASE_URL.
-DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
+#DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
